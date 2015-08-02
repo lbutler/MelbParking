d22c3897b5766351b37d68fec68b37ca5a1631ac
Luke Butler <lukepbutler@gmail.com>
2015-08-02 12:20:18 +1000

    Adding menu controls
    Early days will link up and style shortly

--- js/ParkingTimeControl.js
+++ js/ParkingTimeControl.js
@@ -6,7 +6,9 @@ L.Control.ParkingTimeControl = L.Control.extend({
 
     div.innerHTML = '<h1>Thursday</h1>' +
     '<h2>12th June 2014</h2>' +
-    '<h1><i class="fa fa-clock-o"></i> <span id="time">12:00 am</span><h1>';
+    '<h1><i class="fa fa-clock-o"></i> <span id="time">12:00 am</span><h1>' +
+    '<hr>'+
+    '<i class="fa fa-calendar-o"></i><i class="fa fa-clock-o"></i><i class="fa fa-area-chart"></i><i class="fa fa-info-circle"></i>';
 
     return div;
     },
